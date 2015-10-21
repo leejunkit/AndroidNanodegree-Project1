ab018372f074c6377c0cf3516ede72f64e72b7c7
Lee Jun Kit <mohawk.riceball@gmail.com>
2015-10-21 18:02:36 +0800

    refactor MovieDetailFragment into its own class

--- app/src/main/java/co/x22media/popularmovies/MovieDetailActivity.java
+++ app/src/main/java/co/x22media/popularmovies/MovieDetailActivity.java
@@ -1,20 +1,11 @@
 package co.x22media.popularmovies;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.squareup.picasso.Picasso;
-
-import java.util.Calendar;
-
+import co.x22media.popularmovies.fragments.MovieDetailFragment;
 import co.x22media.popularmovies.models.Movie;
 
 
@@ -33,7 +24,7 @@ public class MovieDetailActivity extends ActionBarActivity {
 
             // create the fragment and put the Movie object inside
             // (I still can't believe I can't simply pass objects via constructor parameters)
-            PlaceholderFragment frag = new PlaceholderFragment();
+            MovieDetailFragment frag = new MovieDetailFragment();
             Bundle b = new Bundle();
             b.putParcelable("movieParam", m);
             frag.setArguments(b);
@@ -64,49 +55,5 @@ public class MovieDetailActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-
-            Movie m = getArguments().getParcelable("movieParam");
-
-            View rootView = inflater.inflate(R.layout.fragment_movie_detail, container, false);
-
-            // find the ImageView
-            ImageView iv = (ImageView)rootView.findViewById(R.id.movie_poster_image_view);
-            Picasso.with(getActivity())
-                    .load(m.getPosterURL())
-                    .placeholder(R.drawable.poster_placeholder)
-                    .into(iv);
-
-            // find the Title TextView
-            TextView titleTextView = (TextView)rootView.findViewById(R.id.movie_title_text_view);
-            titleTextView.setText(m.getTitle());
-
-            // find the Release Date TextView
-            TextView releaseDateTextView = (TextView)rootView.findViewById(R.id.movie_release_date_text_view);
-            Calendar c = Calendar.getInstance();
-            c.setTime(m.getReleaseDate());
-            releaseDateTextView.setText(String.valueOf(c.get(Calendar.YEAR)));
-
-            // find the Rating TextView
-            TextView ratingTextView = (TextView)rootView.findViewById(R.id.movie_rating_text_view);
-            ratingTextView.setText(String.valueOf(m.getUserRating()) + " out of 10");
-
-            // find the Synopsis TextView
-            TextView synopsisTextView = (TextView)rootView.findViewById(R.id.movie_synopsis_text_view);
-            synopsisTextView.setText(m.getSynopsis());
-            return rootView;
-        }
     }
 }
---
+++ app/src/main/java/co/x22media/popularmovies/fragments/MovieDetailFragment.java
@@ -0,0 +1,60 @@
+package co.x22media.popularmovies.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.Calendar;
+
+import co.x22media.popularmovies.R;
+import co.x22media.popularmovies.models.Movie;
+
+/**
+ * Created by kit on 21/10/15.
+ */
+public class MovieDetailFragment extends Fragment {
+
+    public MovieDetailFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        Movie m = getArguments().getParcelable("movieParam");
+
+        View rootView = inflater.inflate(R.layout.fragment_movie_detail, container, false);
+
+        // find the ImageView
+        ImageView iv = (ImageView)rootView.findViewById(R.id.movie_poster_image_view);
+        Picasso.with(getActivity())
+                .load(m.getPosterURL())
+                .placeholder(R.drawable.poster_placeholder)
+                .into(iv);
+
+        // find the Title TextView
+        TextView titleTextView = (TextView)rootView.findViewById(R.id.movie_title_text_view);
+        titleTextView.setText(m.getTitle());
+
+        // find the Release Date TextView
+        TextView releaseDateTextView = (TextView)rootView.findViewById(R.id.movie_release_date_text_view);
+        Calendar c = Calendar.getInstance();
+        c.setTime(m.getReleaseDate());
+        releaseDateTextView.setText(String.valueOf(c.get(Calendar.YEAR)));
+
+        // find the Rating TextView
+        TextView ratingTextView = (TextView)rootView.findViewById(R.id.movie_rating_text_view);
+        ratingTextView.setText(String.valueOf(m.getUserRating()) + " out of 10");
+
+        // find the Synopsis TextView
+        TextView synopsisTextView = (TextView)rootView.findViewById(R.id.movie_synopsis_text_view);
+        synopsisTextView.setText(m.getSynopsis());
+        return rootView;
+    }
+}
